1672db731049e7369228621c39efa4c2451bacda
Sheharyar Saif <sherysaif786@gmail.com>
2022-08-19 04:01:50 +0500

    Initial Commit

---
+++ Contacts App/Contacts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_AppApp.swift
+//  Contacts App
+//
+//  Created by Sheharyar Saif on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
